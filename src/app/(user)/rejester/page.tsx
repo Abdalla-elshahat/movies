5229c9a676d8383b98867e4515f2b074d50227ad
abdalla <abdalla@54>
2024-09-16 16:27:42 +0300

    Second commit

--- src/app/(user)/rejester/page.tsx
+++ src/app/(user)/rejester/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { useTranslation } from "react-i18next"
+import "../sin.css"
 import RegisterForm from "./RegisterForm"
 const RegisterPage = () => {
   const {t}=useTranslation();
   return (
     <section className="fix-height container m-auto px-7 flex items-center justify-center">
-      <div className="m-auto bg-white rounded-lg p-5 w-full md:w-2/3 maindiv">
+      <div className="m-auto rounded-lg p-5 w-full md:w-2/3 maindiv">
         <h1 className="text-3xl font-bold text-gray-800 mb-5">
           {t("Create New Account")}
         </h1>
